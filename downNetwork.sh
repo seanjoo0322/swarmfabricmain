4435196b91d759e91047dfdc43dd17ce3e1d13d6
Ubuntu <gbchoi@CGB-DWP-VM3.o150q4aycasuxmfbonm3q1fike.syx.internal.cloudapp.net>
2024-01-15 04:54:48 +0000

    modified downNetowkr

--- downNetwork.sh
+++ downNetwork.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 echo "Which network are you going to down?"
 echo "Type 1 or 2"
 read network
 
-docker-compose -f docker-compose-testingnetwork$network.yaml down
+if [ "$network" == "1" ]; then
+    docker-compose -f docker-compose-testingnetwork1.yaml down
+    docker volume rm swarmfabric_orderer.example.com swarmfabric_peer1.org1.example.com swarmfabric_peer0.org1.example.com
+elif [ "$network" == "2" ]; then
+    docker-compose -f docker-compose-testingnetwork2.yaml down
+    docker volume rm swarmfabric_orderer2.example.com swarmfabric_peer1.org2.example.com swarmfabric_peer0.org2.example.com
+else
+    echo "Invalid choice. Please type 1 or 2."
+fi
