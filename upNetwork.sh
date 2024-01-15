736fbc24c2542ab58b1a8393764273c3d46ff366
Ubuntu <gbchoi@CGB-DWP-VM3.o150q4aycasuxmfbonm3q1fike.syx.internal.cloudapp.net>
2024-01-15 05:59:22 +0000

    orderer fix

--- upNetwork.sh
+++ upNetwork.sh
@@ -2,6 +2,6 @@ echo "Which network are you going to up?"
 echo "Type 1 or 2"
 read network
 
-docker-compose -f docker-compose-testingnetwork$network.yaml up 
+docker-compose -f docker-compose-testingnetwork$network.yaml up -d
 
 docker ps -a
