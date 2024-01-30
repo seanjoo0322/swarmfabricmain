d3eb059abe7f43aedbe1e583eb4794c32f906456
Hyun Seo Joo(Sean Joo) <112134402+seanjoo0322@users.noreply.github.com>
2024-01-30 14:32:04 +0900

    Update chaincode.sh

--- chaincode.sh
+++ chaincode.sh
@@ -7,7 +7,7 @@ infoln() {
 infoln "Type in your chaincode file name(Ex)chaincode-go): "
 read chaincodeFile
 
-infoln "Type in the name of your chaincode"
+infoln "Type in the name of your chaincode Ex) basic"
 read chaincodeName
 
 infoln "Step 1: Packaging Chaincode"
@@ -70,6 +70,7 @@ done
 infoln "Type in your channel Name"
 read channelName
 
+infoln "Step 2: Approving"
 infoln "APPROVE org1"
 docker exec cli peer lifecycle chaincode approveformyorg --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --channelID ${channelName} --name ${chaincodeName} --version 1 --sequence 1 --waitForEvent --package-id $PACKAGEIDORG1
 
@@ -96,13 +97,72 @@ done
 infoln "Checking Commit Readiness if not true true, ur doomed"
 docker exec cli peer lifecycle chaincode checkcommitreadiness --channelID ${channelName} --name ${chaincodeName} --version 1 --sequence 1
 
-infoln "Commiting Chaincode.." 
-docker exec cli peer lifecycle chaincode commit -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt --channelID mychannel --name basic --version 1 --sequence 1
-
 exit 0;
+
+
+infoln "Step 3: Commiting Chaincode.." 
+infoln "Enter the number of organizations:"
+read numOrgs
+
+# Initialize an array to store organization data
+orgData=()
+
+# Loop to collect data for each organization
+for ((i=1; i<=$numOrgs; i++))
+do
+    infoln "Enter the name of Organization $i:"
+    read orgName
+
+    infoln "Enter the name of peer0 for Organization $i:"
+    read peer0Name
+
+    infoln "Enter the port for peer0 of Organization $i:"
+    read portName
+
+    # Construct the peer address and TLS root certificate file options
+    peerAddress="--peerAddresses ${peer0Name}.${orgName}.example.com:${portName}"
+    tlsRootCertFile="--tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/${orgName}.example.com/peers/${peer0Name}.${orgName}.example.com/tls/ca.crt"
+
+    # Add organization data to the array
+    orgData+=("${peerAddress}" "${tlsRootCertFile}")
+done
+
+# Join organization data with spaces to form command options
+peerAddresses=$(printf " %s" "${orgData[@]::1}")  # Extract peerAddresses
+tlsRootCertFiles=$(printf " %s" "${orgData[@]:1}")  # Extract tlsRootCertFiles
+
+# Construct the final command with dynamic options
+cmd="docker exec cli peer lifecycle chaincode commit \
+-o orderer.example.com:7050 \
+--tls \
+--cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+${peerAddresses} \
+${tlsRootCertFiles} \
+--channelID ${channelName} \
+--name basic \
+--version 1 \
+--sequence 1"
+
+invoke="docker exec cli peer chaincode invoke \
+-o orderer2.example.com:8050 \
+--tls true \
+--cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+-C space \
+-n basic \
+${peerAddresses} \
+${tlsRootCertFiles} \
+-c '{\"function\":\"InitLedger\",\"Args\":[]}'"
+
+# Print the final command
+eval "$cmd"
 
 infoln "Checking if commit is complete"
 docker exec cli peer lifecycle chaincode querycommitted --channelID ${channelName} --name ${chaincodeName}
 
+infoln "Invoking main code" 
+eval "$invoke" 
+
+
+
 infoln "Invoking main code"
-docker exec cli peer chaincode invoke -o orderer2.example.com:8050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n basic --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
+docker exec cli peer chaincode invoke -o orderer2.example.com:8050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C space -n basic --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
