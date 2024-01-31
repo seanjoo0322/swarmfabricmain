4cbb70332bbf658850d2c76cc7dbb2dec5df533c
Hyun Seo Joo(Sean Joo) <112134402+seanjoo0322@users.noreply.github.com>
2024-01-31 09:42:11 +0900

    Update chaincode.sh

--- chaincode.sh
+++ chaincode.sh
@@ -161,6 +161,3 @@ infoln "Invoking main code"
 eval "$invoke" 
 
 
-
-infoln "Invoking main code"
-docker exec cli peer chaincode invoke -o orderer2.example.com:8050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C space -n basic --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
